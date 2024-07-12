93f9c1a71911240bcc680525ac03d134403c4cd4
ziyesss <choijiyea7185@gmail.com>
2024-07-12 17:54:09 +0900

    CRUDAPI-create

---
+++ src/main/java/com/zeze/board/controller/PostController.java
@@ -0,0 +1,46 @@
+package com.zeze.board.controller;
+
+import com.zeze.board.model.Post;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+//Controller: API와 서버와 클라이언트가 소통하는 직접적인 창구
+@RestController
+public class PostController {
+
+    @GetMapping("/api/v1/posts")
+    public ResponseEntity<List<Post>> getPosts() {
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1L, "Post 1", ZonedDateTime.now()));
+        posts.add(new Post(2L, "Post 2", ZonedDateTime.now()));
+        posts.add(new Post(3L, "Post 3", ZonedDateTime.now()));
+
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/v1/posts/{postId}")
+    public ResponseEntity<Post>getPost(
+            @PathVariable Long postId
+    ) {
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1L, "Post 1", ZonedDateTime.now()));
+        posts.add(new Post(2L, "Post 2", ZonedDateTime.now()));
+        posts.add(new Post(3L, "Post 3", ZonedDateTime.now()));
+
+        Optional<Post> matchingPost =
+                posts.stream().filter(post -> postId.equals(post.getPostId())).findFirst();
+
+        return matchingPost
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+}
---
+++ src/test/java/com/zeze/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeze.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
